92e7faa2c35a350eb286124519c67e9631ae7d8a
Quixotic95 <e.oguz95@hotmail.com>
2021-09-21 05:41:51 +0300

    Add, Configure and Implement SpringFox Swagger to Project

--- credit-app/src/main/java/com/gittigidiyor/quxiotic95/creditapp/CreditAppApplication.java
+++ credit-app/src/main/java/com/gittigidiyor/quxiotic95/creditapp/CreditAppApplication.java
@@ -2,8 +2,10 @@ package com.gittigidiyor.quxiotic95.creditapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CreditAppApplication {
 
     public static void main(String[] args) {
