2ec8a54ea3250aa1ec20db8d0ba76f803d1ac361
Quixotic95 <e.oguz95@hotmail.com>
2021-09-21 05:43:48 +0300

    Edit Imports & Edit Annotations

--- credit-app/src/main/java/com/gittigidiyor/quxiotic95/creditapp/dto/ExceptionLogDTO.java
+++ credit-app/src/main/java/com/gittigidiyor/quxiotic95/creditapp/dto/ExceptionLogDTO.java
@@ -1,15 +1,12 @@
 package com.gittigidiyor.quxiotic95.creditapp.dto;
 
 import com.gittigidiyor.quxiotic95.creditapp.dto.generic.GenericDTO;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.Instant;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+@Getter
+@Setter
 public class ExceptionLogDTO implements GenericDTO<ExceptionLogDTO> {
 
     private Instant timestamp;
