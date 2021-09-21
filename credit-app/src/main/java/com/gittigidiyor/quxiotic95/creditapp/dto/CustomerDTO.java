bb8a6a3a3d4b972522255e77c3cccb42004d5919
Quixotic95 <e.oguz95@hotmail.com>
2021-09-21 05:48:06 +0300

    Implement Swagger2 ApiModelProperty & Fix at Regular Expressions

--- credit-app/src/main/java/com/gittigidiyor/quxiotic95/creditapp/dto/CustomerDTO.java
+++ credit-app/src/main/java/com/gittigidiyor/quxiotic95/creditapp/dto/CustomerDTO.java
@@ -4,7 +4,9 @@ import com.gittigidiyor.quxiotic95.creditapp.dto.generic.GenericDTO;
 import com.gittigidiyor.quxiotic95.creditapp.validation.TCKNConstraint;
 import com.gittigidiyor.quxiotic95.creditapp.validation.group.FirstCheck;
 import com.gittigidiyor.quxiotic95.creditapp.validation.group.SecondCheck;
-import lombok.*;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.validator.constraints.Range;
 
 import javax.validation.constraints.NotBlank;
@@ -15,22 +17,27 @@ import javax.validation.constraints.Pattern;
 @Setter
 public class CustomerDTO implements GenericDTO<CustomerDTO> {
 
+    @ApiModelProperty(example = "A. Emre")
     @NotBlank(message = "First name is required!")
-    @Pattern(regexp = "^[\\p{L}]+([(\\s)$]?+[\\p{L}]+)+", message = "First Name is incorrect!")
+    @Pattern(regexp = "^[\\p{L}]+[.]?([(\\s)$]?+[\\p{L}]+[.]?)+", message = "First Name is incorrect!")
     private String firstName;
 
+    @ApiModelProperty(example = "Oğuz")
     @NotBlank(message = "Last name is required!")
     @Pattern(regexp = "^[\\p{L}]+([(\\s)$]?+[\\p{L}]+)+", message = "Last Name is incorrect!")
     private String lastName;
 
+    @ApiModelProperty(example = "7000")
     @NotNull(message = "Monthly income must be specified!")
     @Range(min = 0, message = "Monthly income must be minimum 0!")
     private Double monthlyIncome;
 
-    @Pattern(regexp = "^(5)([0-9]{2})\\s?[0-9]{3}(\\s?([0-9]{2})){2}$", message = "Phone Number format is wrong!")
+    @ApiModelProperty(example = "5413711566")
+    @Pattern(regexp = "^(5)[0-9]{2}[0-9]{3}[0-9]{4}$", message = "Phone Number format is wrong!")
     @NotBlank(message = "Phone number is required!")
     private String phoneNumber;
 
+    @ApiModelProperty(example = "23570297588")
     @Pattern(regexp = "^[1-9][0-9]{10}$", message = "TCKN can not start with 0 and must contain 11 numbers!", groups = FirstCheck.class)
     @TCKNConstraint(groups = SecondCheck.class)
     @NotBlank(message = "TCKN is mandatory!")
