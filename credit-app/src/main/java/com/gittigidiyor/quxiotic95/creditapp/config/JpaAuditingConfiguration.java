492e4032aa84fe4dc59ed47ac0e8837cc6ccbb18
Quixotic95 <e.oguz95@hotmail.com>
2021-09-21 05:38:15 +0300

    BUGFIX - Add EnableJpaAuditing configuration
    -> Null @CreatedDate and @LastModifiedDate

---
+++ credit-app/src/main/java/com/gittigidiyor/quxiotic95/creditapp/config/JpaAuditingConfiguration.java
@@ -0,0 +1,10 @@
+package com.gittigidiyor.quxiotic95.creditapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+
+}
